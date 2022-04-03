1bff5edded844842374fb79f038a39776bd6e163
devChase <devchase0115@gmail.com>
2022-04-03 21:35:37 +1000

    JS: home transparent

--- main.js
+++ main.js
@@ -29,7 +29,17 @@ contactMe.addEventListener("click", () => {
   scrollIntoView('#contact');
 });
 
+
+// Make home contents transparent when scrolldown
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1-window.scrollY/homeHeight;
+});
+
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior: "smooth"});
-}
+};
